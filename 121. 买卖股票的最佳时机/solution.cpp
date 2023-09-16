3fde51a184b4a750f8b68c407617de7c556a8aa3
Minghao-Liang <liangminghao0810@foxmail.com>
2023-09-16 09:10:48 +0800

    add 121

---
+++ 121. 买卖股票的最佳时机/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxp = 0, minp = 10001;
+        for(int i = 0; i < n; i++) {
+            maxp = max(maxp, prices[i] - minp);
+            minp = min(minp, prices[i]);
+        }
+        return maxp;
+    }
+};
