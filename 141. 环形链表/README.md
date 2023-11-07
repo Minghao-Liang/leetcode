7de89f86426db4cf6e741bab99d4c89ea273f87b
Minghao-Liang <liangminghao0810@foxmail.com>
2023-11-07 15:53:00 +0800

    update 141

--- 141. 环形链表/README.md
+++ 141. 环形链表/README.md
@@ -3,3 +3,5 @@
 [题目：141. 环形链表](https://leetcode.cn/problems/linked-list-cycle/)
 
 该题只需要设置快慢指针即可，当两指针相等且不是空指针时，就存在环。
+
+solution1.cpp和solution2.cpp是两个写法，都使用了双指针，但是2的写法在判断哪一个点是入环的第一个结点时更好用。
---
+++ 141. 环形链表/solution2.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr) {
+            return false;
+        }
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(1) {
+            slow = slow->next;
+            if(fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            fast = fast->next->next;
+            if(fast == slow) break;
+        }
+        return true;
+    }
+};
