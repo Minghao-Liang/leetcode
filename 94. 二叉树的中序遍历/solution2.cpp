284b3ecedef43f8a166fa162fcdb4bf230942022
Minghao-Liang <liangminghao0810@foxmail.com>
2023-11-11 10:27:13 +0800

    add 94

---
+++ 94. 二叉树的中序遍历/solution2.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> re;
+        stack<TreeNode*> stk;
+        TreeNode *node = root;
+        while(node || !stk.empty()) {
+            if(node) {
+                stk.push(node);
+                node = node->left;
+            }
+            else {
+                node = stk.top();
+                re.push_back(node->val);
+                stk.pop();
+                node = node->right;
+            }
+        }
+        return re;
+    }
+};
