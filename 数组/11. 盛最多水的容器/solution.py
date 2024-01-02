d9e06607da06d72657fdbffcd0481199982be57a
Minghao-Liang <liangminghao0810@foxmail.com>
2024-01-02 11:09:54 +0800

    add 11

---
+++ 数组/11. 盛最多水的容器/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        result = 0
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            if area > result: result = area
+            if height[l] < height[r]: l += 1
+            else: r -= 1
+
+        return result
